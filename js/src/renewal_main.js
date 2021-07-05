a1229b8386fe8716f5d98fe3d2a71a0940da858c
ons <https://github.com/On-s/ons>
2021-07-05 18:20:30 +0900

    0705 subpage setting

---
+++ js/src/renewal_main.js
@@ -0,0 +1,38 @@
+
+document.cookie = "SameSite=Lax; Secure";
+
+
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '576',
+    width: '1024',
+    videoId: 'W4WsqeBC47E',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 4000);
+    done = true;
+  } 
+}
+function stopVideo() {
+  player.stopVideo();
+}
