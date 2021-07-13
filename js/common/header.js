f939688a518098ad06de8cff51afb4d30ce619ce
On-s <dhstjdgns42@gmail.com>
2021-07-13 18:19:50 +0900

    sub html 생성

--- js/common/header.js
+++ js/common/header.js
@@ -1,32 +1,41 @@
 //jQuery ------------------------------
 // flie name : header
 //-------------------------------------
-    var search_btn = $('.search_btn');
+var search_btn = $('.search_btn');
 
 
-    console.log(search_btn);
-    function ckStatus(searchbar) {
-        var barwidth = searchbar.width();
-        // var status = Boolean(searchbar.css('display') === 'none');
-        if (barwidth < 100) {
-            searchbar.animate({
-                width : '150px'
-            });
-        } else {
-            searchbar.animate({
-                width : '0px'
-            });
-        }
-    };
+console.log(search_btn);
 
-    // 만일 페이지 호출되고 자바스크립트로 직접만들었을때 문서에 이벤트를 걸어야한다.
-    // 아래와 같이 사용시 반응이없음. 원인은 $(document).ready(function(){ 함수를 통해 페이지가 이미 로드 해오면서 진행이 됬기에 실행이 안되는것
-    // search_btn.on('click',function() {
-    //     console.log('click');
-    // })
-    $(document).on('click','.search_btn', function (e) {
-        e.preventDefault();
-        var searchbar = $('.searchbar');
-        console.log('click search');
-        ckStatus(searchbar);
-    });
+function ckStatus(searchbar) {
+    var barwidth = searchbar.width();
+    // var status = Boolean(searchbar.css('display') === 'none');
+    if (barwidth < 100) {
+        searchbar.animate({
+            width: '300px'
+        });
+    } else {
+        searchbar.val('');
+        searchbar.animate({
+            width: '0px'
+        });
+    }
+};
+
+// 만일 페이지 호출되고 자바스크립트로 직접만들었을때 문서에 이벤트를 걸어야한다.
+// 아래와 같이 사용시 반응이없음. 원인은 $(document).ready(function(){ 함수를 통해 페이지가 이미 로드 해오면서 진행이 됬기에 실행이 안되는것
+// search_btn.on('click',function() {
+//     console.log('click');
+// })
+$(document).on('click', '.search_btn', function (e) {
+    e.preventDefault();
+    var searchbar = $('.searchbar');
+    console.log('click search');
+    ckStatus(searchbar);
+
+// 로그인 버튼 누를 경우 팝업창 띄우기 
+// 팝업창 x 버튼 누르거나 밖에 누르면 끄기
+// 
+
+
+
+});
--- js/src/bhc_main/renewal_main_ad.js
+++ js/src/bhc_main/renewal_main_ad.js
@@ -1,19 +1,6 @@
 //jQuery ------------------------------
 // flie name : renewal_main
 //-------------------------------------
-jQuery.event.special.touchstart = {
-  setup: function (_, ns, handle) {
-    if (ns.includes("noPreventDefault")) {
-      this.addEventListener("touchstart", handle, {
-        passive: false
-      });
-    } else {
-      this.addEventListener("touchstart", handle, {
-        passive: true
-      });
-    }
-  }
-};
 (function ($) {
   $.ajax({
     url: "../res/data/adData.json",
