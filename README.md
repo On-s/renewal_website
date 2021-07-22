35bc4eed8cacc0022d1a02a10b4ef1a714363a41
On-s <dhstjdgns42@gmail.com>
2021-07-22 16:03:37 +0900

    데이터 받기 , 버튼이벤트 활성화

--- README.md
+++ README.md
@@ -10,15 +10,19 @@
 
 */res/guide/Renewal_BHC_Responsive Web.png
 
-main 
-headBox - 공통영역으로 분리하고 반응형으로 구현
-footer - 공통영역으로 분리하고 반응형으로 구현
 
-sub_ckmenu jqeury 와 ajax 를 사용하여 구현
+#### html file )
 
-sub_aboutbhc - jqeury 사용하여 구현
++ main - jqeury 를사용하여 반응형으로 구현
++ headBox - 공통영역으로 분리하고 반응형으로 구현
++ footer - 공통영역으로 분리하고 반응형으로 구현
 
-sub_StartUp - ajax(jquery) , 바닐라 JS 로 구현
++ sub_ckmenu jqeury 와 ajax 를 사용하여 구현
 
-sub_stroe - api 사용 (예정)
++ sub_aboutbhc - jqeury 사용하여 구현
 
++ sub_StartUp - ajax(jquery) , 바닐라 JS 로 구현
+
++ sub_stroe - api 사용 (예정)
+
++ sub_Service - 바닐라 JS 로 구현
--- js/src/sub_Service/bhc_renewal_sub_Service.js
+++ js/src/sub_Service/bhc_renewal_sub_Service.js
@@ -9,7 +9,11 @@ let serviceData;
 const qnaCon = document.querySelector('#qnaCon');
 const qnaBox = qnaCon.querySelector('.qna_box');
 const product = qnaBox.querySelector('.product');
-const order = qnaBox.querySelector('.qnaBox');
+const order = qnaBox.querySelector('.order');
+const menuCon = document.querySelector('#menuCon');
+const proBtn = menuCon.querySelector('.pro_btn');
+const ordBtn = menuCon.querySelector('.ord_btn');
+
 
 // IE 버전이낮으면 불러올수없기에 바꿔줌
 if (window.XMLHttpRequest) {
@@ -18,40 +22,65 @@ if (window.XMLHttpRequest) {
   jsonData = new ActiveXObject("Microsoft.XMLHTTP");
 }
 
-
-function mDl(data,position,count) {
-  for (let i in data) {
+// epdl
+function mDl(data, position) {
+  for (let i = 0; i < data.length; i++) {
     let mDl = document.createElement('dl');
-    datatitle = Object.keys(data)[count]; 
-    a = data.Product[i].question;
-    console.log(a);
-    let con = `<dt>${data.Product[i].question}</dt><dd>${data.Product.answer}</dd>`;
+    let con = `<dt>${data[i].question}</dt><dd>${data[i].answer}</dd>`;
+    console.log(data[i].question);
     mDl.innerHTML = con;
     position.appendChild(mDl);
   }
 }
 
+// display 상태체크 함수 
+function clickchange(data1, data2) {
+  let data1st = data1.style;
+  let data2st = data2.style;
+
+  let getdata1attr = data1.getAttribute('display');
+  let getdata2attr = data2.getAttribute('display');
+
+  if (getdata1attr === 'none' && getdata2attr === 'block') {
+    data1.style.display = 'block';
+    data2.style.display = 'none';
+  }
+  data1st.display = 'none';
+  data2st.display = 'block';
+}
+
+
 // Data 불러오기 
 jsonData.onreadystatechange = function () {
-  
+
   if (jsonData.readyState === 4 && jsonData.status === 200) {
     console.log('success');
+
     serviceData = JSON.parse(jsonData.responseText);
 
-    console.log(serviceData.Product[0].question);
-    console.log(serviceData.Product[0].answer);
+    const productData = serviceData.Product;
+    const orderData = serviceData.Order;
 
-    // let mDl = document.createElement('dl');
-    // let con = `<dt>${serviceData.Order[0].question}</dt><dd>${serviceData.Order[0].answer}</dd>`
-    // mDl.innerHTML = con;
-    // product.appendChild(mDl);
+    mDl(productData, product);
+    mDl(orderData, order);
 
-    mDl(serviceData,product,0)
+    proBtn.addEventListener('click',function(e) {
+      e.preventDefault();
+      clickchange(order, product);
+    });
+
+    ordBtn.addEventListener('click',function(e) {
+      e.preventDefault();
+      clickchange(product, order);
+    });
   }
 }
 
 jsonData.open("GET", "../res/data/serviceData.json");
 jsonData.send();
+
+
+
 
 
 // console.log(jsonData.responseText);
