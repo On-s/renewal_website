b183fb845429992517de5c4f6a165077a78867cc
On-s <dhstjdgns42@gmail.com>
2021-07-23 11:33:36 +0900

    api test

--- js/src/sub_stroe/map_kakao.js
+++ js/src/sub_stroe/map_kakao.js
@@ -1,15 +1,18 @@
 // https://apis.map.kakao.com/web/guide/
 
+
+
+const REST_API_KEY = '4ac0100e2dd3015d00795ce6c881021c';
  $.ajax({
-        url: 'https://dapi.kakao.com/v2/local/search/address.json?query=' + encodeURIComponent('제주'),
-        headers: {'Authorization' : 'KakaoAK 4ac0100e2dd3015d00795ce6c881021c'},
+        url: 'https://dapi.kakao.com//v2/local/search/address.json?query=제주',
+        headers: {'Authorization' : 'KakaoAK' + REST_API_KEY},
         type : 'GET'
     }).done(function (data) {
 
         console.log(data);
         var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
         var options = { //지도를 생성할 때 필요한 기본 옵션
-            center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+            center: new kakao.maps.LatLng(126.498229141199, 33.4889179032603), //지도의 중심좌표.
             level: 3 //지도의 레벨(확대, 축소 정도)
         };
         
